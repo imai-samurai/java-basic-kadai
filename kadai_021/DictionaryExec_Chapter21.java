1008043d226b479088d21c85fbc9d278d1e96fb5
imai-samurai <tomatov2222@gmail.com>
2026-06-02 02:05:15 +0900

    課題２１追加

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,12 @@
+package kadai.kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		
+		Dictionary_Chapter21 zisyo = new Dictionary_Chapter21();
+		String[] word = {"apple", "banana", "grape", "orange"};
+		zisyo.checkWord(word);
+		
+	}
+}
---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,33 @@
+package kadai.kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	
+	HashMap<String,String> map = new HashMap<String,String>();
+	
+	public Dictionary_Chapter21() {
+		map.put("apple","りんご");
+		map.put("peach","桃");
+		map.put("banana","バナナ");
+		map.put("lemon","レモン");
+		map.put("pear","梨");
+		map.put("kiwi","キウイ");
+		map.put("strawberry","いちご");
+		map.put("grape","ぶどう");
+		map.put("mascat","マスカット");
+		map.put("cherry","さくらんぼ");
+	}
+	public void checkWord(String[] searchWord) {
+		
+		for(String word : searchWord) {
+			
+			if(map.containsKey(word)){
+				System.out.println(word + "の意味は" + map.get(word));
+			} else {
+				System.out.println(word + "は辞書には存在しません");
+			}
+			
+		}
+	}
+}
